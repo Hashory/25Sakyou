4f00791cdf47b5a4b34ec607e0b25e0f9187b6f2
Hashory <89839285+Hashory@users.noreply.github.com>
2025-04-27 09:19:42 +0900

    Refactoring the `itemManager.cs`

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -7,109 +7,26 @@ public class ItemManager : MonoBehaviour
     [SerializeField] private List<BoxCollider2D> spawnList = new List<BoxCollider2D>();
     [SerializeField] private float spawnTime;
     [SerializeField] private float spawnInterval;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private static readonly Vector2[] Directions = { Vector2.down, Vector2.right, Vector2.left, Vector2.up };
+
     void Start()
     {
         spawnTime = 0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
         spawnTime += Time.deltaTime;
         if (spawnInterval < spawnTime)
         {
-            //上下左右どこに生成されるか
             int num = Random.Range(0, spawnList.Count);
-            //どのアイテムが生成されるか
             int itemNum = Random.Range(0, items.Count);
             BoxCollider2D spawnRange = spawnList[num];
-
             Vector3 randomRange = RandomRange(spawnRange.bounds);
 
             GameObject item = Instantiate(items[itemNum], randomRange, Quaternion.identity);
-            if (num == 0)
-            {
-                //上に生成
-                if (itemNum == 0)
-                {
-                    //とげ
-                    item.GetComponent<ThornCon>().SetThorn(Vector2.down);
-                }
-                else if (itemNum == 1)
-                {
-                    //リンご
-                    item.GetComponent<AppleCon>().SetApple(Vector2.down);
-                }
-                else if (itemNum == 2 || itemNum == 3)
-                {
-                    //爆弾
-                    item.GetComponent<BombCon>().SetThorn(Vector2.down);
-                }
-            }
-
-            if (num == 1)
-            {
-                //左に生成
-                if (itemNum == 0)
-                {
-                    //とげ
-                    item.GetComponent<ThornCon>().SetThorn(Vector2.right);
-                }
-                else if (itemNum == 1)
-                {
-                    //リンご
-                    item.GetComponent<AppleCon>().SetApple(Vector2.right);
-                }
-                else if (itemNum == 2 || itemNum == 3)
-                {
-                    //爆弾
-                    item.GetComponent<BombCon>().SetThorn(Vector2.right);
-                }
-
-            }
-
-            if (num == 2)
-            {
-                //→に生成
-                if (itemNum == 0)
-                {
-                    //とげ
-                    item.GetComponent<ThornCon>().SetThorn(Vector2.left);
-                }
-                else if (itemNum == 1)
-                {
-                    //リンご
-                    item.GetComponent<AppleCon>().SetApple(Vector2.left);
-                }
-                else if (itemNum == 2 || itemNum == 3)
-                {
-                    //爆弾
-                    item.GetComponent<BombCon>().SetThorn(Vector2.left);
-                }
-
-            }
-
-            if (num == 3)
-            {
-                //↓に生成
-                if (itemNum == 0)
-                {
-                    //とげ
-                    item.GetComponent<ThornCon>().SetThorn(Vector2.up);
-                }
-                else if (itemNum == 1)
-                {
-                    //リンご
-                    item.GetComponent<AppleCon>().SetApple(Vector2.up);
-                }
-                else if (itemNum == 2 || itemNum == 3)
-                {
-                    //爆弾
-                    item.GetComponent<BombCon>().SetThorn(Vector2.up);
-                }
-
-            }
+            SetItemDirection(item, itemNum, Directions[num]);
             spawnTime = 0f;
         }
     }
@@ -118,7 +35,29 @@ public class ItemManager : MonoBehaviour
     {
         float randomX = Random.Range(bounds.min.x, bounds.max.x);
         float randomY = Random.Range(bounds.min.y, bounds.max.y);
-
         return new Vector3(randomX, randomY, 0);
+    }
+
+    private void SetItemDirection(GameObject item, int itemNum, Vector2 direction)
+    {
+        // itemNum: 0=Thorn, 1=Apple, 2/3=Bomb
+        if (itemNum == 0)
+        {
+            // Thorn
+            var thorn = item.GetComponent<ThornCon>();
+            if (thorn != null) thorn.SetThorn(direction);
+        }
+        else if (itemNum == 1)
+        {
+            // Apple
+            var apple = item.GetComponent<AppleCon>();
+            if (apple != null) apple.SetApple(direction);
+        }
+        else if (itemNum == 2 || itemNum == 3)
+        {
+            // Bomb
+            var bomb = item.GetComponent<BombCon>();
+            if (bomb != null) bomb.SetThorn(direction);
+        }
     }
 }
